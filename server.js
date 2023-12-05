1f8bbfad2d3fd70e681f02c737914006f480acf5
fireab <Freab4@gmail.com>
2023-12-05 09:03:34 +0300

    passport.js

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-const users = [];
 
 const express = require("express");
+const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
-const initializePassport = require("./passport-config");
-const passport = require("passport");
+const methodOverride = require("method-override");
 
+const initializePassport = require("./passport-config");
 initializePassport(
   passport,
   (email) => users.find((user) => user.email === email),
   (id) => users.find((user) => user.id === id)
 );
 
-const app = express();
+const users = [];
 
-app.use(flash());
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,18 +29,21 @@ app.use(
     saveUninitialized: false,
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
+app.get("/", checkAuthenticated, (req, res) => {
   res.render("index.ejs", { name: req.user.name });
 });
-app.get("/login", (req, res) => {
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
+
 app.post(
   "/login",
+  checkNotAuthenticated,
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
@@ -47,11 +51,11 @@ app.post(
   })
 );
 
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
@@ -64,9 +68,35 @@ app.post("/register", async (req, res) => {
   } catch {
     res.redirect("/register");
   }
-  console.log(users);
 });
 
+app.delete("/logout", (req, res) => {
+  req.logOut((err) => {
+    if (err) {
+      // Handle error, if any
+      console.error(err);
+      res.status(500).send("Error logging out");
+      return;
+    }
+    res.redirect("/"); // Redirect after successful logout
+  });
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
 app.listen(5000, () => {
-  console.log("server is runnning on port 5000");
+  console.log("server is running on port 3000");
 });
